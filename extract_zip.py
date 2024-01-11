64ea1310e0045eb9dba31df5ee5a4ff614ab7636
time <270185054@qq.com>
2024-01-12 00:36:39 +0800

    refactoring code

---
+++ extract_zip.py
@@ -0,0 +1,69 @@
+import os
+import zipfile
+import rarfile
+import concurrent.futures
+
+
+def read_passwords(password_file):
+    with open(password_file, encoding='utf-8') as f:
+        return list(set(map(str.strip, f)))
+
+
+class CompressionHandler:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.file_type = file_path.split('.')[-1].lower()
+        self.unzip2path = os.path.splitext(file_path)[0]
+        self.is_encrypted = self.check_encryption()
+
+    def check_encryption(self):
+        try:
+            if self.file_type in ["zip", "uvz"]:
+                with zipfile.ZipFile(self.file_path) as zp:
+                    for l in zp.infolist():
+                        if l.flag_bits & 0x1:
+                            return True
+            elif self.file_type == "rar":
+                with rarfile.RarFile(self.file_path, mode='r') as zp:
+                    if zp.needs_password():
+                        return True
+            else:
+                print("Unsupported file type: %s" % self.file_type)
+                exit(0)
+        except Exception as e:
+            print("Error checking encryption:", e)
+        return False
+
+    def extract_file(self, password=''):
+        try:
+            if self.file_type in ["zip", "uvz"]:
+                with zipfile.ZipFile(self.file_path, 'r') as zip_file:
+                    zip_file.extractall(path=self.unzip2path, pwd=password.encode('utf-8'))
+            elif self.file_type == "rar":
+                with rarfile.RarFile(self.file_path, 'r') as rar_file:
+                    rar_file.extractall(path=self.unzip2path, pwd=password.encode('utf-8'))
+            print("Real password is: %s" % password)
+        except Exception as e:
+            if password:
+                print(f"Test password: {password}, failed!")
+            else:
+                print(f"An error occurred for {self.file_path}: {e}")
+
+    def decrypt_and_extract(self, password_file="passwords.txt"):
+        print('Encrypted! Trying to decrypt it with Passbook.')
+        pwd_lists = read_passwords(password_file)
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.extract_file, pwd) for pwd in pwd_lists]
+
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    future.result()
+                except Exception as e:
+                    print(f"Thread error: {e}")
+
+    def extract(self):
+        if not self.is_encrypted:
+            self.extract_file()
+        else:
+            self.decrypt_and_extract()
--- pdg_converter.py
+++ pdg_converter.py
@@ -5,77 +5,77 @@ from PIL import Image
 from PyPDF2 import PdfFileMerger, PdfFileWriter
 import tkinter as tk
 from tkinter import filedialog
-from decrypt_zip import extract
+from extract_zip import CompressionHandler
 
-# 遍历 文件目录下所有文件--函数
+
 def walk_os_file(path):
+    # 遍历文件目录下所有文件
     result = []
     for root, dirs, files in os.walk(path):
         for file in files:
             perpath = os.path.join(root, file)
             result.append(perpath.replace("\\", "/"))
     return result
-    
+
+
 def pdg2pdf(pdg_path):
     # 单页pdg转化为pdf
-    new_path = pdg_path.replace(os.path.splitext(pdg_path)[1],'.pdf')
+    new_path = pdg_path.replace(os.path.splitext(pdg_path)[1], '.pdf')
     try:
         img = Image.open(pdg_path)
         # 使用RGB模式，减少原有格式占用的空间
         img = img.convert('RGB')
-        img.save(new_path, "PDF",quality=75, subsampling=0, resolution=100.0, save_all=True)
-    except:
-        pass
+        img.save(new_path, "PDF", quality=75, subsampling=0, resolution=100.0, save_all=True)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-def mergePDF(path, book_name='temp.pdf'):
+
+def mergePDF(extracted_path, book_name='temp.pdf'):
     print('开始合并文件...')
-    paths = [i for i in os.listdir(path) if i.endswith('pdf')]
+    paths = [i for i in os.listdir(extracted_path) if i.endswith('pdf')]
     # 对图书页码进行排序
-    pdfs = sorted(sorted(paths),key=lambda x: (x[0].isdigit(),x[0]=='!',x[0]=='f',x[0]=='l',x[0]=='b',x[0]=='c'))
-
+    pdfs = sorted(sorted(paths),
+                  key=lambda x: (x[0].isdigit(), x[0] == '!', x[0] == 'f', x[0] == 'l', x[0] == 'b', x[0] == 'c'))
     merger = PdfFileMerger()
     for pdf in pdfs:
         try:
-            file = os.path.join(path,pdf)
+            file = os.path.join(extracted_path, pdf)
             merger.append(open(file, 'rb'))
-        except:
-            print(pdf+'----error!')
-    merger.write(os.path.join(path,book_name))
+        except Exception as e:
+            print(f"An error occurred for {pdf}: {e}")
+    merger.write(os.path.join(extracted_path, book_name))
     merger.close()
     print('合并完成！')
 
 
-    
 def convert(ziped_file):
     print('解压结束，开始转化格式....')
     # 解压缩的文件所在路径
     extracted_path = os.path.splitext(ziped_file)[0]
     # 保存路径，只是将压缩文件路径改为pdf
-    save_path = ziped_file.replace(os.path.splitext(ziped_file)[1],'.pdf')
+    save_path = ziped_file.replace(os.path.splitext(ziped_file)[1], '.pdf')
     # 判断是否有下级文件夹
-    if len(os.listdir(extracted_path))<=3:
-        extracted_path = os.path.join(extracted_path,os.listdir(extracted_path)[0])
+    if len(os.listdir(extracted_path)) <= 3:
+        extracted_path = os.path.join(extracted_path, os.listdir(extracted_path)[0])
     # 读取解压目录下所有文件
     pdg_paths = walk_os_file(extracted_path)
     # 将每个pdg文件转化为pdf，并删除原来的文件。
     for pdg_path in pdg_paths:
         pdg2pdf(pdg_path)
-    #合并所有页面至一个pdf文档
-    mergePDF(extracted_path,save_path)
+    # 合并所有页面至一个pdf文档
+    mergePDF(extracted_path, save_path)
     # 删除加压的文件夹
     # shutil.rmtree(extracted_path)
-        
+
+
 if __name__ == '__main__':
-    
     # 文件选择器选择文件
     root = tk.Tk()
     root.withdraw()
 
     # Folderpath = filedialog.askdirectory() #获得选择好的文件夹
-    ziped_file = filedialog.askopenfilename() #获取图书压缩包
-    extract(ziped_file)
+    ziped_file = filedialog.askopenfilename()  # 获取图书压缩包
+    # 解压解密文档
+    CompressionHandler(ziped_file).extract()
+    # 转化为pdf
     convert(ziped_file)
-    
-    
-    
-
